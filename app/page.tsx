fda4c735d6185ec59552b42ca77ca704af01994a
mrsolodev <hking4152@gmail.com>
2025-01-16 21:03:16 +0530

    Scrollable Story Section Implementation

--- app/page.tsx
+++ app/page.tsx
@@ -12,29 +12,33 @@ import BottomNavbar from "@/components/bottomnav/BottomNavbar";
 import Navbar from "@/components/navbar/Navbar";
 import fetchPost from "./api/fetchPost";
 import { useAppSelector } from "./lib/store/hooks";
+import fetchStories from "./api/fetchStories";
+
+type Story = {
+  content: string;
+  time: string;
+};
 
 
 export default function Home() {
+  const user = userData.fetchUser();
+  const currentUser = useAppSelector((state) => state.users.items);
 
-  const user = userData.fetchUser()
-  const currentUser = useAppSelector(state => state.users.items)
-
-  const feed = useAppSelector(state => state.posts.items)
+  const feed = useAppSelector((state) => state.posts.items);
+  const { stories, storiesCount } = fetchStories();
 
   if (user.username == undefined) {
-    redirect("/login")
+    redirect("/login");
   }
 
-  // alert(JSON.stringify(feed[0]));
-  
-
   function postInteraction(id: any, interactionType: string) {
-    let Id: number = parseInt(id) 
-    
-    if (interactionType === 'like') console.log("liked");
-    else if (interactionType === 'save') console.log('saved post to collection');
-    else if (interactionType === 'share') console.log('shared post to xyz');
-    else if (interactionType === 'comment') console.log('commented!!')
+    let Id: number = parseInt(id);
+
+    if (interactionType === "like") console.log("liked");
+    else if (interactionType === "save")
+      console.log("saved post to collection");
+    else if (interactionType === "share") console.log("shared post to xyz");
+    else if (interactionType === "comment") console.log("commented!!");
   }
 
   return (
@@ -51,36 +55,31 @@ export default function Home() {
             isOwner={true}
             isWatched={false}
           />
-          <Story
-            username={userData.UserList[2].username}
-            image={userData.UserList[2].image}
-            isOwner={false}
-            isWatched={false}
-          />
-          <Story
-            username={userData.UserList[1].username}
-            image={userData.UserList[1].image}
-            isOwner={false}
-            isWatched={true}
-          />
+          {stories.map((item: any, index: any) => (
+            <Story
+              key={index}
+              username={item.author.name}
+              image={item.author.image}
+              isOwner={false}
+              isWatched={index != 0}
+            />
+          ))}
         </Stories>
 
-        { feed.map((item: any, index: any) => (
-              <Post
-                userId={item.userId}
-                key={index}
-                caption={item.caption}
-                timePosted={item.timePosted}
-                author={item.author}
-                image={item.image}
-                action={postInteraction}
-                id={index}
-                likedUser={item.likedUser}
-              />
-            ))
-          }
+        {feed.map((item: any, index: any) => (
+          <Post
+            userId={item.userId}
+            key={index}
+            caption={item.caption}
+            timePosted={item.timePosted}
+            author={item.author}
+            image={item.image}
+            action={postInteraction}
+            id={index}
+            likedUser={item.likedUser}
+          />
+        ))}
       </Feed>
-
 
       <section className="w-[100px] h-full hidden"></section>
     </main>
